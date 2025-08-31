e599f31b5d2ff7e826b59a5883d68da48d615adb
kaushar Ansari <kausharansari9373@gmail.com>
2025-08-31 12:59:12 +0530

    add new file

---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+    void display(vector<int> &v){
+        for(int i = 0; i<v.size(); i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+int main () {
+
+ vector<int> v = {1,2,3,4,5,6,7,8,9};
+ display(v);
+ 
+
+
+
+ return 0;
+}
